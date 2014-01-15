8a09c8395502f896f8ec03e2ca889549463515fc
zaza275 <zhaheqing@qq.com>
2014-01-16 00:08:52 +0800

    lesson2_ActionBar
    not done, can't show the serach button on the action bar...

--- MyFirstApp/gen/com/example/myfirstapp/R.java
+++ MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -21,9 +21,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_search=0x7f080002;
         public static final int action_settings=0x7f080001;
         public static final int edit_message=0x7f080000;
     }
@@ -34,15 +36,17 @@ public final class R {
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int main_activity_actions=0x7f070002;
     }
     public static final class string {
+        public static final int action_search=0x7f050004;
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050002;
         public static final int edit_message=0x7f050001;
-        public static final int hello_world=0x7f050006;
-        public static final int title_activity_display_message=0x7f050005;
-        public static final int title_activity_main=0x7f050004;
+        public static final int hello_world=0x7f050007;
+        public static final int title_activity_display_message=0x7f050006;
+        public static final int title_activity_main=0x7f050005;
     }
     public static final class style {
         /** 
--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -23,8 +25,26 @@ public class MainActivity extends Activity
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle press on the action bar items
+		switch (item.getItemId())
+		{
+			case R.id.action_search:
+				openSearch();
+				return true;
+			case R.id.action_settings:
+				openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 	/** Called when the user clicks the Send button */
